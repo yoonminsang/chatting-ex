9b66dbad4924341c60f08832754b75b3e31dda9c
yoonminsang <apdlyooapdl@naver.com>
2022-05-16 00:34:22 +0900

    feat: user repository에 회원가입 추가

--- packages/server/src/module/auth/user.repository.ts
+++ packages/server/src/module/auth/user.repository.ts
@@ -1,5 +1,11 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from '../../entity/user.entity';
+import { SignupDto } from './dto/signup.dto';
 
 @EntityRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  async createUser(signupDto: SignupDto) {
+    const user = this.create(signupDto);
+    await this.save(user);
+  }
+}
