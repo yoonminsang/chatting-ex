6ad3666118c78616076d4b34b3fec01f395d1d60
yoonminsang <apdlyooapdl@gmail.com>
2022-05-15 22:41:28 +0900

    feat: app module 설정

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from '../typeorm.config';
+import { AuthModule } from './module/auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: typeORMConfig,
+    }),
+    AuthModule,
+  ],
 })
 export class AppModule {}
